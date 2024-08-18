368fcd92e54de16a2d93bfcf459f615cd92e24e4
Mateus <mateuseandrade@gmail.com>
2024-08-18 20:28:33 -0300

    Primeiro metodo do projeto

---
+++ src/main/java/br/com/mateusandrade/rest_with_spring_boot_and_java_erudio/Greeting.java
@@ -0,0 +1,19 @@
+package br.com.mateusandrade.rest_with_spring_boot_and_java_erudio;
+
+public class Greeting {
+  private final long id;
+  private final String content;
+
+  public Greeting(long id, String content) {
+    this.id = id;
+    this.content = content;
+  }
+  
+  public long getId() {
+    return id;
+  }
+  public String getContent() {
+    return content;
+  }
+
+}
